6e31d40014bb8e468bb1a25c277a813ad8103348
Dimitri Providas <dimitri@sierramonitor.com>
2017-10-01 19:16:44 +0200

    - Updating package.json

--- transactionDetails.js
+++ transactionDetails.js
@@ -1,12 +1,3 @@
-//todo: change these to a random account on luno
-//todo: make a random account and transfer 100 rands worth.
-
-// email: d.providas@gmail.com
-// password: u79GH9ykonMJ
-
-//frq4d8aw9xc6s
-//JQD-6oSEedagQpFSGWLcYvXC7Pz3EW6LwDEkhlfHb7M
-
 function TransactionDetailsClass() {
   this.transactionDetails = {
     keyID: 'nj9sbq46gvbuh',
