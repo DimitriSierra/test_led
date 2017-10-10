f7aca01d3fa6ae745581e0c64fb77bf596c17081
Ronillitus <ronald.trades@gmail.com>
2017-10-10 22:48:10 +0200

    Changed user details

--- userDefined.js
+++ userDefined.js
@@ -1,13 +1,13 @@
 function UserDefinedClass() {
   this.transactionDetails = {
-    keyID: '-',
-    secretID: '-'
+    keyID: 'jryayzr5dzckq',
+    secretID: 'RazZMEieTsbBCkBfvofLydrGyqMurXRsrNc8aMQ3D0g'
   };
   this.debug = false;
   this.tradingVolume = '0.001';
   this.orderRandHistory = 80;
   this.ratioBuy = 0.80;
-  this.ratioSell = 0.55;
+  this.ratioSell = 0.50;
   this.hardLimit = 100;
 }
 
