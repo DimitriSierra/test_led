d6e861b0b3bfaf8aa6582f75088645c0069ae2fb
Ronillitus <ronald.trades@gmail.com>
2017-10-31 22:49:37 +0200

    Fixed the maxBuffer issue

--- luno.js
+++ luno.js
@@ -11,7 +11,7 @@ function LunoClass() {
 
   this.getTicker = function getTicker(callback) {
     var command = 'curl https://api.mybitx.com/api/1/ticker?pair=XBTZAR';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       result = JSON.parse(result);
       callback(err, result);
@@ -20,7 +20,7 @@ function LunoClass() {
 
   this.getOrderBook = function getOrderBook(callback) {
     var command = 'curl https://api.mybitx.com/api/1/orderbook?pair=XBTZAR';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       result = JSON.parse(result);
       callback(err, result);
@@ -29,7 +29,7 @@ function LunoClass() {
 
   this.getTrades = function getTrades(callback) {
     var command = 'curl https://api.mybitx.com/api/1/trades?pair=XBTZAR';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       result = JSON.parse(result);
       callback(err, result);
@@ -39,7 +39,7 @@ function LunoClass() {
   this.getBalances = function getBalances(apiObject, callback) {
     var command = 'curl -u ' + apiObject.keyID + ':' + apiObject.secretID;
     command += ' https://api.mybitx.com/api/1/balance';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       if (result.indexOf('Unauthorized') !== -1) return callback(result);
       result = JSON.parse(result);
@@ -50,7 +50,7 @@ function LunoClass() {
   this.getListOrders = function getListOrders(apiObject, callback) {
     var command = 'curl -u ' + apiObject.keyID + ':' + apiObject.secretID;
     command += ' https://api.mybitx.com/api/1/listorders';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       if (result.indexOf('Unauthorized') !== -1) return callback(result);
       result = JSON.parse(result);
@@ -62,7 +62,7 @@ function LunoClass() {
     var command = 'curl -u ' + apiObject.keyID + ':' + apiObject.secretID + ' -X POST -d pair=XBTZAR';
     command += ' -d type=' + bidOrAsk + ' -d volume=' + orderVolume + ' -d price=' + orderPrice;
     command += ' https://api.mybitx.com/api/1/postorder';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       console.log('Luno1: ' + err);
       console.log('---');
       console.log('Luno2: ' + JSON.stringify(err));
@@ -81,7 +81,7 @@ function LunoClass() {
   this.setStopAnOrder = function setStopAnOrder(apiObject, orderID, callback) {
     var command = 'curl -u ' + apiObject.keyID + ':' + apiObject.secretID + ' -X POST -d order_id=';
     command += orderID + ' https://api.mybitx.com/api/1/stoporder';
-    exec(command, function (err, result) {
+    exec(command, {maxBuffer:Infinity}, function (err, result) {
       if (err) return callback(err);
       if (result.indexOf('Unauthorized') !== -1) return callback(result);
       if (result.indexOf('Invalid order') !== -1) return callback(result);
