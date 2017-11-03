1b1732abf8b212cc83a4f011623ffd76b2989029
Dimitri <dimitri@sierramonitor.com>
2017-11-03 07:40:10 +0200

    Issue-2- Commiting everything

--- userDefined.js
+++ userDefined.js
@@ -1,12 +1,12 @@
 function UserDefinedClass() {
   this.transactionDetails = {
-    keyID: '-',
-    secretID: '-'
+    keyID: 'dpft9j8wfm9kv',
+    secretID: 'trDsDL6ynQ8-4Wvmxox-cWphASyUo3vZzzQDekdS1D4'
   };
   this.debug = false;
-  this.tradingVolume = '0.02';
+  this.tradingVolume = '0.0005';
   this.orderRandHistory = 80;
-  this.ratioBuy = 0.75;
+  this.ratioBuy = 0.20;
   this.ratioSell = 0.50;
   this.hardLimit = 100;
   this.ratioRepeat = 1;
